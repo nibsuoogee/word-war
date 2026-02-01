5ba24fe747329afc80cdac1c47484512e7cc7f34
nibsuoogee <elias.syyrila@gmail.com>
2026-02-01 18:22:00 +0200

    Husky and playwright

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,7 +5,18 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      // suppress errors for missing 'import React' in files (Preact doesn't need React imported)
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ]);
--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,6 @@
-import { useState } from "preact/hooks";
 import "./app.css";
 
 export function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Word War</h1>
